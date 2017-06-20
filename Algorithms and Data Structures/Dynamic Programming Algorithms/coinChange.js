68a7d4d4dfe67f814a52c9d9928b398b8720c97d
Tiffany <tiffanylin27@gmail.com>
2017-06-19 18:24:42 -0700

    Add brief explanation of the algorithm

--- Algorithms and Data Structures/Dynamic Programming Algorithms/coinChange.js
+++ Algorithms and Data Structures/Dynamic Programming Algorithms/coinChange.js
@@ -14,3 +14,22 @@ function coinChange (n, c) {
 
 let result = coinChange(100, [1, 5, 10, 25]);
 console.log(result);
+
+// initialize memo[0] to 1 (there is only 1 way to make 0)
+// 1 - coin(1) = 0;
+// memo[1] += memo[0];
+
+// 2 - coin(1) = 1;
+// memo[2] += memo[1];
+// 3 - coin(1) = 2;
+// memo[3] += memo[2];
+
+// ...
+
+// 5 - coin(5) = 0;
+// memo[5] += memo[0]; // memo[5] = 1;
+// 5 - coin(1) = 4;
+// memo[5] += memo[4]; // memo[5] = 2;
+// There are two ways to make 5 cents with the given coins: 
+// 1 + 1 + 1 + 1 + 1
+// 5
