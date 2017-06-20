8b2cff25ef80873341d2828a2c53ed49ba630a84
Tiffany <tiffanylin27@gmail.com>
2017-06-19 18:24:27 -0700

    Add iterative approach

--- Algorithms and Data Structures/Algorithms on Strings/stringPermutations.js
+++ Algorithms and Data Structures/Algorithms on Strings/stringPermutations.js
@@ -11,16 +11,44 @@ let stringPermutations = (s, p = '', result = []) => {
   return result;
 }
 
-let s = 'ab';
+let s = 'hello';
 // for (let i = 0; i < s.length; i++) {
   // console.log(s.substring(0, i));
   // console.log(s.substring(i + 1, s.length));  
 //   console.log(s.substring(0, i) + s.substring(i + 1, s.length));  
 // }
 
-let result = stringPermutations(s);
-console.log(result.sort());
+// let result = stringPermutations(s);
+// console.log(result.sort());
 
-let stringIterative = (string) => {
-  
+let stringIterative = (k, string) => {
+  let result = [string];
+  let s = string.split('');
+  for(let i = 1; i < s.length; i++) {
+    swap(s, k % (i + 1), i); 
+    result.push(s.join(''))
+    k = k / (i + 1);
+  }
+  return result;
 }
+
+function factorialize(num) {
+  if (num === 0 || num === 1)
+    return 1;
+  for (var i = num - 1; i >= 1; i--) {
+    num *= i;
+  }
+  return num;
+};
+
+function swap (a, i, j) {
+  let temp = a[i];
+  a[i] = a[j];
+  a[j] = temp;
+}
+
+let k = factorialize(s.length);
+let iterativeResult = stringIterative(k, s);
+console.log(iterativeResult.sort());
+
+
