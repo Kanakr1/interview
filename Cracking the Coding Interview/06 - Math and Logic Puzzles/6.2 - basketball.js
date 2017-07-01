9dfaa6b744786559f4521ab9b2cc4b092acdddc4
Tiffany <tiffanylin27@gmail.com>
2017-07-01 14:23:31 -0700

    Add template file for basketball math/logic problem

---
+++ Cracking the Coding Interview/06 - Math and Logic Puzzles/6.2 - basketball.js
@@ -0,0 +1,6 @@
+// You have a basketball hoop and someone says that you can play one of two games.
+// Game 1: You get one shot to tmake the hoop.
+// Game 2: You get three tshots and you have to make tw of three shots.
+
+// If p is the probabliity for making a particular shot, for which values of p should you pick one game or the other?
+
